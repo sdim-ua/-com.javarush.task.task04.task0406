229004648ab3c531f625e725dd34fe365f5307cb
sdim-ua <ingersol@yandex.ua>
2019-01-15 00:05:10 +0400

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,18 @@
+/* 
+Программа учета имен
+*/
+
+public class Cat {
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+       
+        //напишите тут ваш код
+        this.fullName = fullName;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
